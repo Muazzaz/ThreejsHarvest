e36e8b31fe0885258e971173c1ea7c321f5634a0
Muazzaz <hasan.almuazzazruet@gmail.com>
2026-07-18 22:28:56 +0600

    feat: add physical tree colliders for collision detection

--- src/components/scene/Orchard.tsx
+++ src/components/scene/Orchard.tsx
@@ -1,12 +1,23 @@
+import { CylinderCollider, RigidBody } from '@react-three/rapier';
+import { getTerrainHeight } from '../../lib/terrain';
 import { TREE_PLACEMENTS } from '../../lib/products';
 import FruitTree from './FruitTree';
 
 export default function Orchard() {
   return (
     <group>
-      {TREE_PLACEMENTS.map((tree, i) => (
-        <FruitTree key={i} type={tree.type} x={tree.x} z={tree.z} />
-      ))}
+      {TREE_PLACEMENTS.map((tree, i) => {
+        const groundY = getTerrainHeight(tree.x, tree.z);
+        return (
+          <group key={i}>
+            <FruitTree type={tree.type} x={tree.x} z={tree.z} />
+            {/* Invisible trunk collider so the car can't drive through */}
+            <RigidBody type="fixed" position={[tree.x, groundY + 1.2, tree.z]} colliders={false}>
+              <CylinderCollider args={[1.2, 0.35]} />
+            </RigidBody>
+          </group>
+        );
+      })}
     </group>
   );
 }
